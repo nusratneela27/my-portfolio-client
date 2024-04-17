bc244340cff1b87cf54c59cc30df9d947b736eaa
neela <nusratneela27@gmail.com>
2024-04-18 03:59:39 +0600

    new portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,30 +1,50 @@
-import React, { useEffect } from 'react';
-import './About.css'
-import pc from '../../assets/pc.gif'
-import AOS from 'aos';
+import React, { useEffect } from "react";
+import "./About.css";
+import pc from "../../assets/pc.gif";
+import AOS from "aos";
 
 const About = () => {
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
 
-        return () => {
-            AOS.refresh();
-        };
-    }, []);
+    return () => {
+      AOS.refresh();
+    };
+  }, []);
 
-    return (
-        <div id='about' className='grid grid-cols-1 lg:grid-cols-2 gap-5 px-16 md:px-40 pt-20 pb-20 bg-blue-gray-50'>
-            <div className='m-auto ' data-aos="fade-right" data-aos-duration="1000"
-                data-aos-easing="ease-in-sine">
-                <h1 className='text-3xl font-bold text-purple-300 mb-6'>About Me</h1>
-                <p className='text-xl'>I am a passionate and creative Front-End Web Developer. I am familiar with a number of web development technologies, including HTML, CSS, JavaScript, React JS, Express JS, and MongoDB. I finished a few amazing full-stack projects. I'm ready to use my one year of MERN Stack experience.</p>
-            </div>
-            <div className='m-auto'>
-                <img src={pc} className='pc' />
-            </div>
+  return (
+    <div id="about" className="px-16 md:px-40 pt-20 pb-20">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5  border border-gray-400 rounded-2xl p-10">
+        <div
+          className="m-auto "
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-sine"
+        >
+          <h1 className="text-4xl font-light mb-6">About Me</h1>
+          <div className="text-justify text-xl space-y-4">
+            <p>
+              I'm a talented and enthusiastic web developer. Many web
+              development technologies, such as HTML, CSS, JavaScript, React JS,
+              Express JS, and MongoDB, are familiar to me. I completed a few
+              incredible full-stack initiatives. I'm prepared to put my year of
+              web programming experience to use.
+            </p>
+            <p>
+              Seeking chances to advance as a software engineer so I can produce
+              higher caliber web applications and more. I'd be happy to work
+              from home, but I'm also open to moving to a US or European
+              country.
+            </p>
+          </div>
         </div>
-    );
+        <div className="m-auto">
+          <img src={pc} className="pc" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,71 +1,55 @@
-import React from 'react';
-import img from '../../assets/girl.gif'
-import Typed from 'typed.js';
-import { Button } from '@material-tailwind/react';
-import { useEffect } from 'react';
-import AOS from 'aos';
-import cv from "../../assets/resume.pdf"
+import React from "react";
+import img from "../../assets/desktop.jpg";
+import { Button } from "@material-tailwind/react";
+import { useEffect } from "react";
+import AOS from "aos";
+import cv from "../../assets/resume.pdf";
 
 const Home = () => {
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
 
-        return () => {
-            AOS.refresh();
-        };
-    }, []);
+    return () => {
+      AOS.refresh();
+    };
+  }, []);
 
-    const el = React.useRef(null);
-
-    React.useEffect(() => {
-        const typed = new Typed(el.current, {
-            strings: [' Frontend Developer.', ' React Developer.', ' MERN Stack Developer.',],
-            typeSpeed: 90,
-            loop: 3,
-        });
-        return () => {
-            typed.destroy();
-        };
-    }, []);
-
-    // const handleDownload = () => {
-    //     const url = '/src/assets/Resume of neela.pdf';
-    //     const anchor = document.createElement("a");
-    //     anchor.href = url;
-    //     anchor.download = "resume.pdf";
-    //     anchor.click();
-    // }
-
-    return (
-        <div id='/' className='grid grid-cols-1 lg:grid-cols-2 gap-5 px-16 md:px-40 pt-32 pb-12'>
-            <div className='m-auto' data-aos="fade-down" data-aos-duration="3000" data-aos-anchor-placement="top-center">
-                <img src={img} />
-            </div>
-            <div className='mt-20'>
-                <h1 className='text-6xl font-bold text-blue-gray-800'>
-                    <span ref={el}></span>
-                </h1>
-
-                {/* <div>
-                    <a download href="https://drive.google.com/file/d/1GsU0Drg4orIk_h4CoOQNakyrnFGhSS_x/view?usp=drive_link">
-                        <Button className='bg-purple-300 mt-14'>Resume</Button>
-                    </a>
-                </div> */}
-
-                <div>
-                    <a href={cv} download=''>
-                        <Button className='bg-blue-gray-800  mt-14'>Download Resume</Button>
-                    </a>
-                </div>
-
-                {/* <Button className='bg-purple-300 mt-14' onClick={handleDownload}>
-                    Resumesss
-                </Button> */}
-
-            </div>
+  return (
+    <div
+      id="/"
+      className="grid grid-cols-1 lg:grid-cols-2 gap-5 px-16 md:px-40 pt-32 pb-12"
+    >
+      <div
+        className="m-auto"
+        data-aos="fade-down"
+        data-aos-duration="3000"
+        data-aos-anchor-placement="top-center"
+      >
+        <img height={500} width={500} src={img} />
+      </div>
+      <div className="mt-20">
+        <h1 className="text-sm mb-2 italic">{"Hello, My name"}</h1>
+        <h1 className="text-4xl font-semibold">Nusrat Jahan Neela</h1>
+        <h2 className="text-xl italic mt-2">
+          I design and build things for the web
+        </h2>
+        <p className="text-justify mt-4 pe-10">
+          I'm a passionate web developer with a passion for JavaScript and a
+          solid understanding of React.js. My goal is to make amazing user
+          experiences. I use clear, effective code to make concepts come to
+          life.
+        </p>
+        <div>
+          <a href={cv} download="">
+            <Button className="bg-cyan-500 bg-opacity-50 text-black  mt-14">
+              Download Resume
+            </Button>
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
